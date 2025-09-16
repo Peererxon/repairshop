8a62f3b124e835a5523a3290e6e2bdd823ccb912
andersongil <andersongil.dev@gmail.com>
2025-09-16 17:17:56 -0400

    feat: actualizar middleware para mejorar la coincidencia de rutas excluyendo más patrones

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,6 +15,18 @@ export default withAuth(
 
 export const config = {
 	matcher: [
-		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - api (API routes)
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - auth
+		 * - favicon.ico (favicon file)
+		 * - robots.txt
+		 * - images
+		 * - login
+		 * - homepage (represented with $ after beginning /)
+		 */
+		"/((?!api|_next/static|_next/image|auth|favicon.ico|robots.txt|images|login|$).*)",
 	],
 };
